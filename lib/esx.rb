e9520823e7ae2e9a52392687df6987e06e474b61
Harry Yeh <harryyeh@cometcomputing.com>
2012-12-05 22:50:55 -0800

    Fixed to keep the same prototype, added opts{} parameter at the end of self.connect

--- lib/esx.rb
+++ lib/esx.rb
@@ -35,8 +35,9 @@ module ESX
     # Requires hostname/ip, username and password
     #
     # Host connection is insecure by default
-    def self.connect(host, user, password,  opts = {},insecure = true)
+   def self.connect(host, user, password,  insecure = true, opts = {})
       templates_dir = opts[:templates_dir] || "/vmfs/volumes/datastore1/esx-gem/templates"
+     
       vim = RbVmomi::VIM.connect :host => host, :user => user, :password => password, :insecure => insecure
       host = Host.new(host, user,password, :templates_dir => templates_dir)
       host.vim = vim
